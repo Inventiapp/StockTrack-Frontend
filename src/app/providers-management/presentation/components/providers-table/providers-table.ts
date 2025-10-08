b21f12dc4a4bf3f07e7c2644ea278710154abed1
Dayro <dayro.rios.p@gmail.com>
2025-10-07 22:05:47 -0500

    feat(providers): added providers table with shown data

---
+++ src/app/providers-management/presentation/components/providers-table/providers-table.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+import {TranslatePipe} from '@ngx-translate/core';
+import {ProvidersApi} from '../../../infrastructure/providers-api';
+import {Provider} from '../../../../inventory/domain/model/provider.entity';
+
+@Component({
+  selector: 'app-providers-table',
+  imports: [
+    MatTable,
+    MatSort,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    TranslatePipe,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef
+  ],
+  templateUrl: './providers-table.html',
+  styleUrl: './providers-table.css'
+})
+export class ProvidersTable {
+  displayedColumns: string[] = ['firstName', 'phone', 'email', 'ruc'];
+  dataSource: Provider[] = [];
+
+  constructor(private providersApi: ProvidersApi) {}
+
+  ngOnInit(): void {
+    this.providersApi.getProviders().subscribe((providers: Provider[]) => {
+      this.dataSource = providers;
+    });
+  }
+}
+
+// const ElementData = [
+//   {firstName: 1, phone: 'Hydrogen', email: 1.0079, ruc: 'H'},
+// ]
