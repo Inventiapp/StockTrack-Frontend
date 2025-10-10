a93f5c9788674ca1edc6a3fbf4119ccf09c69eac
Dayro <dayro.rios.p@gmail.com>
2025-10-10 12:23:54 -0500

    feat(sales): add sales component with basic layout and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { HomeComponent } from './shared/presentation/views/home/home';
 import { InventoryListComponent } from './inventory/presentation/inventory-list/inventory-list';
 import {ProvidersTable} from './providers-management/presentation/components/providers-table/providers-table';
 import {ProviderComponent} from './shared/presentation/views/provider/provider';
+import {SalesComponent} from './shared/presentation/views/sales/sales';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'inventario', component: InventoryListComponent },
   { path: 'proveedores', component: ProviderComponent },
-  { path: 'venta', component: HomeComponent },
+  { path: 'venta', component: SalesComponent },
   { path: 'reportes', component: HomeComponent },
   { path: 'configuracion', component: HomeComponent },
   { path: 'perfil', component: HomeComponent },
